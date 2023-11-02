8e3734b08d1beda8127167d47f559b91075db3b6
keti.khetsuriani <keti.khetsuriani@softchef.io>
2023-11-02 21:29:35 +0400

    firebase

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCjrBt-I86SP6AcN04SxgAvArmCpepN_84",
+  authDomain: "clone-55586.firebaseapp.com",
+  projectId: "clone-55586",
+  storageBucket: "clone-55586.appspot.com",
+  messagingSenderId: "478022144834",
+  appId: "1:478022144834:web:23c70312ae7306241a6edb",
+  measurementId: "G-E6268CC88E",
+};
